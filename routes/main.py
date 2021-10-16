a68ef5ad15d201a7fb256a76bc7035908873d3cc
Zubir <zubir@zubir.dev>
2021-10-16 04:20:41 -0400

    implement new routes and systemd script

--- routes/main.py
+++ routes/main.py
@@ -1,7 +1,10 @@
+import os
+import random
 from flask import (
         Flask, 
         send_from_directory,
         render_template,
+        url_for,
         )
 
 app = Flask('mk3supra', static_url_path='', static_folder='static', template_folder='templates')
@@ -10,6 +13,22 @@ app = Flask('mk3supra', static_url_path='', static_folder='static', template_fol
 def hello_world():
         return render_template('index.html')
 
-@app.route("/another")
-def rello_world():
-        return "<p>Hello, Sorld!</p>"
+@app.route("/external-docs")
+def external_docs():
+        return render_template('external-docs.html')
+
+@app.route("/external-parts")
+def external_parts():
+        return render_template('external-parts.html')
+
+@app.route("/brochure-art")
+def art_ep():
+        art_folder = 'img/brochure'
+        filenames = os.listdir(os.path.join(app.static_folder, art_folder))
+        paths = [url_for('static', filename=os.path.join(art_folder, f)) for f in filenames]
+        random.shuffle(paths)
+        return render_template('brochure-art.html', art=paths)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
--- serve-dev.py
+++ serve-dev.py
@@ -2,6 +2,7 @@ import werkzeug.serving
 from routes.main import app
 
 def run_server():
+    app.debug = True
     werkzeug.run_simple(application=app, hostname='127.0.0.1', port=1989, use_reloader=True)
 
 if __name__ == '__main__':
