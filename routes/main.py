cd090849c39d110d55e19e4dd094c04628082fff
Zubir <zubir@zubir.dev>
2021-10-20 02:32:16 -0400

    add file upload and public/private templates

--- routes/main.py
+++ routes/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c9e5616322867794bab3bebc9f4abc2d19365e350715c151ca91a8c0bd48057
-size 1235
+oid sha256:62cb475f23e9b299a65daf5da869ba2bf6d1e44dcaab96d1b6d885b1db79a731
+size 2162
