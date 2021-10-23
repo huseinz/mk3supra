45898eba8d80530797fc80b8e9370926ff89402b
Zubir <zubir@zubir.dev>
2021-10-22 23:52:17 -0400

    start AC writeup and add markdown support

--- routes/main.py
+++ routes/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62cb475f23e9b299a65daf5da869ba2bf6d1e44dcaab96d1b6d885b1db79a731
-size 2162
+oid sha256:c24d680996f69b10c353d5b0af0830f90b0850e814f0f1330259290e397ccc4a
+size 4362
---
+++ templates/public/markdown/ac-restoration.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:118319e7c7f16a84907aabe5b59002497e57dffc238501f59ec91eb0a9200074
+size 6907
